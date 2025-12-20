06926b76f842c085b1f0d2b1a9cffaab6b21f8ff
Rajadurai31 <kit27.csbs45@gmail.com>
2025-12-20 18:43:12 +0530

    Packages updated

---
+++ src/test/java/com/example/campus_pulse/CampusPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
